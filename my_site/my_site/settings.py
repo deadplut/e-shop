85821babc4bca8cdca9bc3bffff5a49c8ccc0c36
Boris Suganov <59098721+deadplut@users.noreply.github.com>
2022-07-07 02:15:05 +0300

    ver. 0.3.0
    Добавлено:
    - создание
    -удаление
    -редактирование постов для авторизованных пользователей
    
    - редактор профился
    - автоматическое создание профился
    - сохранение изображений профился

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +130,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'shop-home'
 LOGIN_URL = 'login'
+
+
+# для сохранения картинок
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- my_site/shop/models.py
+++ my_site/shop/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-# Create your models here.
+from django.urls import reverse
 
 
 
@@ -14,3 +14,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
